a6bf461382206a0aae69141a3b4d81fa83c5de78
Yuki <fabbanana1980@gmail.com>
2019-08-06 07:48:58 +0000

    Finish 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '02965c58eff79012c6d9158ba3fef6b716b4eb1b69472369765c66b197ffbb4c9da7d31a95c470a9cb9cf2df9d57ac727ed284ccf8e632b4910c105e322c4f0e'
